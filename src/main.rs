82b26a0e6655078232593a2b77e0e52d112d7940
Nikolajus Krauklis <nikolajus@gmail.com>
2024-02-23 19:19:59 +0100

    building ast

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use nom::{
-    branch::alt, bytes::{complete::{tag, tag_no_case, take_until}, streaming::tag_no_case}, character::complete::{alpha1, alphanumeric1, char, line_ending, multispace0, one_of, space0}, combinator::{eof, map, map_res, recognize}, complete::take, error::VerboseError, multi::{many0, many0_count, many_till}, sequence::{delimited, pair, preceded, separated_pair, tuple}, Err, IResult
+    branch::alt,
+    bytes::{
+        complete::{tag, tag_no_case, take_until},
+    },
+    character::complete::{alpha1, alphanumeric1, char, line_ending, multispace0, one_of, space0},
+    combinator::{eof, map, map_res, recognize},
+    complete::take,
+    error::VerboseError,
+    multi::{many0, many0_count, many_till},
+    sequence::{delimited, pair, preceded, separated_pair, tuple},
+    Err, IResult,
 };
 
 use std::{collections::HashMap, error::Error};
@@ -8,15 +18,16 @@ type Pair = HashMap<String, String>;
 type AppError = Box<dyn Error>;
 
 #[derive(Debug)]
-enum AstNode {
+enum AstNode<'a> {
     Comparison {
         op: ComparisonOp,
-        name_value: Pair,
+        var: &'a str,
+        val: &'a str,
     },
     BoolExpression {
         op: LogicOp,
-        lhs: Box<AstNode>,
-        rhs: Box<AstNode>,
+        lhs: Box<AstNode<'a>>,
+        rhs: Box<AstNode<'a>>,
     },
 }
 
@@ -55,6 +66,7 @@ impl LogicOp {
         match i.to_lowercase().as_str() {
             "and" | "&&" => LogicOp::And,
             "or" | "||" => LogicOp::Or,
+            _ => unreachable!(),
         }
     }
 }
@@ -89,8 +101,9 @@ fn parse_logic_op(i: &str) -> IResult<&str, LogicOp> {
     Ok((i, LogicOp::from_str(t)))
 }
 
-fn parse_equal(input: &str) -> IResult<&str, (&str, ComparisonOp, &str)> {
-    tuple((space0, parse_comparison_op, space0))(input)
+fn parse_equal(input: &str) -> IResult<&str, ComparisonOp> {
+    let (i, (_, op, _)) = tuple((space0, parse_comparison_op, space0))(input)?;
+    return Ok((i, op));
 }
 
 fn parse_string_value(i: &str) -> IResult<&str, &str> {
@@ -98,35 +111,39 @@ fn parse_string_value(i: &str) -> IResult<&str, &str> {
     Ok((tail, str))
 }
 
-fn parse_assignment(input: &str) -> IResult<&str, Pair> {
+fn parse_comparison(i: &str) -> IResult<&str, AstNode> {
     map(
         tuple((parse_variable_clean_spaces, parse_equal, parse_string_value)),
-        |(var, _, val)| HashMap::from([(var.to_string(), val.to_string())]),
-    )(input)
-}
-
-fn parse_bool_expr_and(i: &str) -> IResult<&str, LogicExpr> {
-    let and = delimited(multispace0, tag_no_case("and"), multispace0);
-    map(
-        separated_pair(parse_assignment, and, parse_assignment),
-        |(p1, p2)| LogicExpr::And(p1, p2),
+        |(var, op, val)| AstNode::Comparison {
+            op,
+            var,
+            val
+        },
     )(i)
 }
 
-fn parse_bool_expr_or(i: &str) -> IResult<&str, LogicExpr> {
-    let or = delimited(multispace0, tag_no_case("or"), multispace0);
-    map(
-        separated_pair(parse_assignment, or, parse_assignment),
-        |(p1, p2)| LogicExpr::Or(p1, p2),
-    )(i)
-}
+// fn parse_bool_expr_and(i: &str) -> IResult<&str, LogicExpr> {
+//     let and = delimited(multispace0, tag_no_case("and"), multispace0);
+//     map(
+//         separated_pair(parse_assignment, and, parse_assignment),
+//         |(p1, p2)| LogicExpr::And(p1, p2),
+//     )(i)
+// }
+//
+// fn parse_bool_expr_or(i: &str) -> IResult<&str, LogicExpr> {
+//     let or = delimited(multispace0, tag_no_case("or"), multispace0);
+//     map(
+//         separated_pair(parse_assignment, or, parse_assignment),
+//         |(p1, p2)| LogicExpr::Or(p1, p2),
+//     )(i)
+// }
 
-fn parse_main(input: &str) -> IResult<&str, Vec<LogicExpr>> {
-    many0(alt((parse_bool_expr_and, parse_bool_expr_or)))(input)
+fn parse_main(i: &str) -> IResult<&str, AstNode> {
+    alt((parse_comparison,))(i)
 }
 
 fn main() -> Result<(), AppError> {
-    let content = r##"street_name = "Random this or that" OR countryCode = "123 NL 123""##;
+    let content = r##"street_name = "Random this or that""##;
 
     let res = parse_main(content)?;
 
