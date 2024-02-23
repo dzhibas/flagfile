e844c859474bab24ab732e2a0c1219b750b4efdc
Nikolajus Krauklis <nikolajus@gmail.com>
2024-02-23 19:19:59 +0100

    recognize

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,29 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{alpha1, alphanumeric1, line_ending, space0},
+    combinator::{eof, recognize},
+    complete::take,
+    multi::many0_count,
+    sequence::{pair, tuple},
+    IResult,
+};
 use std::{collections::HashMap, error::Error};
-use nom::{IResult, bytes::complete::tag, character::{complete::{alphanumeric1, space0, line_ending}}, sequence::tuple, branch::alt};
 
 type Pair = HashMap<String, String>;
 type AppError = Box<dyn Error>;
 
 fn parse_variable(input: &str) -> IResult<&str, &str> {
-    alphanumeric1(input)
+    recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0_count(alt((alphanumeric1, tag("_")))),
+    ))(input)
 }
-fn parse_equal(input: &str) -> IResult<&str, (&str,&str,&str)> {
-    tuple((space0,tag("="),space0))(input)
+fn parse_equal(input: &str) -> IResult<&str, (&str, &str, &str)> {
+    tuple((space0, tag("="), space0))(input)
 }
 fn parse_value(input: &str) -> IResult<&str, &str> {
-    let (i, (v, _)) = tuple((alphanumeric1, line_ending))(input)?;
+    let (i, (v, _)) = tuple((alphanumeric1, alt((line_ending, eof))))(input)?;
     Ok((i, v))
 }
 
@@ -25,9 +37,8 @@ fn parse_main(input: &str) -> IResult<&str, Vec<Pair>> {
 }
 
 fn main() -> Result<(), AppError> {
-    let content = r"street3=Random
-countryCode=NL
-";
+    let content = r"street_name=Random
+countryCode=NL";
 
     let res = parse_main(content)?;
 
