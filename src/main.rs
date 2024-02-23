c1d826722064414b2a7ddf9992dc88f4ed8e961e
Nikolajus Krauklis <nikolajus@gmail.com>
2024-02-23 19:19:59 +0100

    moving this forward

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use nom::{
     character::complete::{alpha1, alphanumeric1, line_ending, space0},
     combinator::{eof, recognize},
     complete::take,
-    multi::many0_count,
+    multi::{many0, many0_count},
     sequence::{pair, tuple},
-    IResult,
+    Err, IResult,
 };
+
 use std::{collections::HashMap, error::Error};
 
 type Pair = HashMap<String, String>;
@@ -19,26 +20,34 @@ fn parse_variable(input: &str) -> IResult<&str, &str> {
         many0_count(alt((alphanumeric1, tag("_")))),
     ))(input)
 }
+
 fn parse_equal(input: &str) -> IResult<&str, (&str, &str, &str)> {
     tuple((space0, tag("="), space0))(input)
 }
+
 fn parse_value(input: &str) -> IResult<&str, &str> {
     let (i, (v, _)) = tuple((alphanumeric1, alt((line_ending, eof))))(input)?;
     Ok((i, v))
 }
 
+fn parse_assignment(input: &str) -> IResult<&str, Pair> {
+    let res = tuple((parse_variable, parse_equal, parse_value))(input);
+    match res {
+        Ok((input, (var, _, val))) => {
+            Ok((input, HashMap::from([(var.to_string(), val.to_string())])))
+        }
+        Err(e) => Err(e),
+    }
+}
+
 fn parse_main(input: &str) -> IResult<&str, Vec<Pair>> {
-    let mut v = Vec::new();
-    let (input, (var, _, val)) = tuple((parse_variable, parse_equal, parse_value))(input)?;
-    v.push(HashMap::from([(var.into(), val.into())]));
-    let (input, (var, _, val)) = tuple((parse_variable, parse_equal, parse_value))(input)?;
-    v.push(HashMap::from([(var.into(), val.into())]));
-    Ok((input, v))
+    many0(parse_assignment)(input)
 }
 
 fn main() -> Result<(), AppError> {
-    let content = r"street_name=Random
-countryCode=NL";
+    let content = r##"street_name =Random
+countryCode =  NL
+demo=demo4"##;
 
     let res = parse_main(content)?;
 
